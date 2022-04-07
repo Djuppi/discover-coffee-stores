268351ffa05a2e3d989983ef91163d8a5f8d0cf1
Aske Djupnes Ammentorp <askeda@gmail.com>
2022-04-07 13:17:11 +0200

    Section 1-3

---
+++ components/banner.js
@@ -0,0 +1,18 @@
+import styles from './banner.module.css';
+
+const Banner = (props) => {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>
+                <span className={styles.title1}>Coffee</span> 
+                <span className={styles.title2}>Connoisseur</span>
+            </h1>
+            <p className={styles.subTitle}>Discover your local coffee shops!</p>
+            <p>
+                <button className={styles.button} onClick={props.handleOnClick}>{props.buttonText}</button>
+            </p>
+        </div>
+    )
+}
+
+export default Banner;
